a97dbf5b46124be2fa7e5d3925913e411a43e03a
Chyp3r <82210412+Chyp3r@users.noreply.github.com>
2022-10-10 22:21:04 +0300

    Add files via upload

---
+++ Kolay-Seviye/Karakter Tersten Yazdırma.cs
@@ -0,0 +1,18 @@
+﻿// .Net Core 6.0
+// Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulama
+// Örnek: Input: Merhaba Hello Question
+// Output: erhabaM elloH uestionQ
+
+Console.WriteLine("Ters hale getirilicek kelimeleri giriniz: ");
+string deger = Console.ReadLine();
+string[] dizi = deger.Split(" ");
+
+for (int i = 0; i < dizi.Length; i++)
+{
+    char[] yenilemedizisi = dizi[i].ToCharArray(0,dizi[i].Length);
+    for (int j = 1; j < dizi[i].Length; j++)
+    {
+        Console.Write(yenilemedizisi[j]);
+    }
+    Console.Write("{0} ",yenilemedizisi[0]);
+}
